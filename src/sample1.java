168139afe18c83a4640c56bea468ebafa56a4e8d
poovarasusampath <42676545+poovarasusampath@users.noreply.github.com>
2018-09-09 05:12:59 -0700

    Update Sample1 file

--- src/sample1.java
+++ src/sample1.java
@@ -2,10 +2,7 @@
 public class sample1 {
 public static void main(String[] args) {
 	while(true){
-		System.out.println("vinayaga");vzvcvxcvx
-		ddvsdgsdg
-		asgdgsdgsdg
-		dgsdgsdgdgsdgsg
-		adgdsgsdgaafafafasgs
+		int data = 0;
+	int data1 = 2; 
 }
 }
